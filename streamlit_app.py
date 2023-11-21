483e0543ce3762290dcd7412613f29dd53ee2c45
anilkumarchandu <151598369+anilkumarchandu@users.noreply.github.com>
2023-11-21 10:02:20 +0530

    Update streamlit_app.py
    added some symbols

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('I love my kids')
-streamlit.header ('My son is naughty guy')
-streamlit.text ('My daughter is lazy')
+streamlit.header ('My son is naughty guy  🥗')
+streamlit.text ('My daughter is lazy 🥑')
