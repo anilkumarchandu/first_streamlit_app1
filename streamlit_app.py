0b490f051bbd0c37838bcb9cb0b9d4cccc19565e
anilkumarchandu <151598369+anilkumarchandu@users.noreply.github.com>
2023-11-21 10:09:26 +0530

    Update streamlit_app.py
    imported pandas and created a new data frame to import the fruit list from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
 streamlit.title('I love my kids')
 streamlit.header ('My son is naughty guy  🥗')
 streamlit.text ('My daughter is lazy 🥑')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
